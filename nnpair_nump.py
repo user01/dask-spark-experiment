3c492aa2324d11127982898e041fda72c7b97c85
Erik Langenborg <erik.langenborg@rseg.com>
2019-08-12 22:49:53 -0400

    Numpy plane detection

--- nnpair_nump.py
+++ nnpair_nump.py
@@ -3,6 +3,163 @@ import numpy as np
 import json
 from rdp import rdp
 
+
+x_ = np.linspace(-10., 10., 7)
+
+points = []
+for x in x_:
+    for y in x_:
+        for z in x_:
+            points.append((x,y,z))
+points = np.stack(points)
+points.shape
+points.tolist()
+
+
+normal = np.array([0,1,0.])
+pt_plane = np.array([0,0,0.])
+pt_forward = np.array([0,1,0.])
+correct_side_sign = np.dot(pt_forward - pt_plane, normal)
+
+pt_test = np.array([0,1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+mask_plane1 = (np.dot(points - pt_plane, normal) * correct_side_sign >= 0)
+points[mask_plane1].tolist()
+
+normal = np.array([-1,-1,-1.])
+pt_plane = np.array([0,3,0.])
+pt_forward = np.array([-1,2,-1.])
+correct_side_sign = np.dot(pt_forward - pt_plane, normal)
+
+pt_test = np.array([0,1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+mask_plane2 = (np.dot(points - pt_plane, normal) * correct_side_sign >= 0)
+points[mask_plane2].tolist()
+points[mask_plane2 & mask_plane1].tolist()
+
+# ####################
+normals = np.array([ # normals that define the plane
+    [0,1,0],
+    [-1,-1,-1.],
+])
+pts_plane = np.array([ # pts fixed on plane
+    [0,0,0],
+    [0,3,0.],
+])
+pts_forward = np.array([ # point on the forward side of the plane
+    [0,1,0],
+    [-1,2,-1.],
+])
+
+def np_dot(x, y, axis=1):
+    return np.sum(x * y, axis=axis)
+# np.tensordot(pts_forward - pts_plane, normals, axes=(1,1)).swapaxes(1,2)
+# correct_side_signs = np.sum((pts_forward - pts_plane) * normals, axis=1)
+correct_side_signs = np_dot(pts_forward - pts_plane, normals, axis=1)
+# np.einsum('ijk,k->ij',pts_forward - pts_plane,normals)
+# correct_side_sign = np.dot(pts_forward - pts_plane, normals)
+
+points.shape
+pts_plane.shape
+points.reshape(-1, 1, 3).shape
+pts_plane.reshape(1, -1, 3).shape
+diff = points.reshape(-1, 1, 3) - pts_plane.reshape(1, -1, 3)
+diff.shape
+normals.shape
+
+normals.reshape(1, -1, 3).shape
+correct_side_signs.shape
+np_dot(diff, normals.reshape(1, -1, 3), axis=2).shape
+correct_side_signs.reshape(1, -1).shape
+masks = np_dot(diff, normals.reshape(1, -1, 3), axis=2) * correct_side_signs.reshape(1, -1) >= 0
+masks.all(axis=1)
+points[masks.all(axis=1)].tolist()
+0
+
+
+
+
+
+
+pt_test = np.array([0,-1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,23321,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+
+normal = np.array([0,1,0.])
+pt_plane = np.array([0,100,0.])
+
+pt_forward = np.array([0,101,0.])
+correct_side_sign = np.dot(pt_forward - pt_plane, normal)
+
+pt_test = np.array([0,1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([0,-1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,23321,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+
+normal = np.array([1,0,0.])
+pt_plane = np.array([100,100,100.])
+
+pt_forward = np.array([101,100,-90.])
+correct_side_sign = np.dot(pt_forward - pt_plane, normal)
+
+pt_test = np.array([101,100,100])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([0,-1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,0])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,-1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,1,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+pt_test = np.array([320,23321,232330])
+np.dot(pt_test - pt_plane, normal) * correct_side_sign > 0
+
+
+
+
 xyz = np.array([
     [0,0,0],
     [0,1,0],
